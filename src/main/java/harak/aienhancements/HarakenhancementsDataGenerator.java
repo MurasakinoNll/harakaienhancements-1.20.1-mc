cf4770101308b98b21e61b834409ffa2185941b1
MurasakinoNll <meekacloneacc@gmail.com>
2024-10-28 17:42:23 +0300

    initial

---
+++ src/main/java/harak/aienhancements/HarakenhancementsDataGenerator.java
@@ -0,0 +1,11 @@
+package harak.aienhancements;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class HarakenhancementsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
